1f064b5ab2d74da7b12cd7a63a4ebe419f8b1653
Jasonh90 <37273260+Jasonh90@users.noreply.github.com>
2019-06-22 14:44:42 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ===================================== 
+                Preloader
+======================================== */
+$(window).on('load', function () { //window object (monitor load event)
+    $('#status').fadeOut();
+    $('#preloader').delay(350).fadeOut();
+});
