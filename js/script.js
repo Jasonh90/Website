694c098a12979fd44a10ff314a44d3785f72f20c
Jason <jh2424@cornell.edu>
2019-07-29 00:58:33 -0400

    navbar half done

--- js/script.js
+++ js/script.js
@@ -113,3 +113,99 @@ $(function () {
         time: 2000
     });
 });
+
+/* ===================================== 
+                Google Map
+======================================== */
+$(window).on('load', function () {
+
+    // Map Variables
+    var addressString = 'The Greater New York Area, NYC';
+    var myLatlng = {
+        lat: 40.757026,
+        lng: -73.986123
+    };
+
+    // 1. Render Map
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 11,
+        center: myLatlng
+    });
+
+    // 2. Add Marker
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: "Click To See Address"
+    });
+
+    // 3. Add Info Window
+    var infowindow = new google.maps.InfoWindow({
+        content: addressString
+    });
+
+    // Show info window when user clicks marker
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
+});
+
+/* ===================================== 
+                Navigation
+======================================== */
+
+/* Show & Hide White Navigation */
+$(function () {
+    
+    // show/hide nav on page load
+    showHideNav();
+    
+    $(window).scroll(function() {
+        
+        // show/hide nav on window's scroll
+        showHideNav();
+        
+    });
+    
+    function showHideNav() {
+        
+        if( $(window).scrollTop() > 50 ) {
+            
+            // Show White Nav
+            $("nav").addClass("white-nav-top");
+            
+            // Show Dark Logo
+            $(".navbar-brand img").attr("src", "img/logo/logo-dark.png");
+        } else {
+            
+            // Hide White Nav
+            $("nav").removeClass("white-nav-top");
+            
+            // Show Normal Logo
+            $(".navbar-brand img").attr("src", "img/logo/logo.png");
+        }
+    }
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
