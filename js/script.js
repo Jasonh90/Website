1ef56b165a0ece38d4e008bebc9ba1bac2db3400
Jason Huang <jhuang0325@gmail.com>
2021-01-06 21:56:11 -0500

    update main page

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
                 Preloader
 ======================================== */
 $(window).on('load', function() { //window object (monitor load event)
-    $('#status').fadeOut();
-    $('#preloader').delay(350).fadeOut('slow');
+    $('#status').delay(500).fadeOut();
+    $('#preloader').delay(850).fadeOut('slow');
 });
 /* ===================================== 
                 Team
